4a2aaf3f77a7370ce56542b8789efe57a6d8b2ca
abhicody <honeydude009@gmail.com>
2024-08-20 16:05:56 +0530

    implement the complete layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+      <body className={`${inter.variable} ${ibmPlexSerif.variable} bg-white`}>
         {children}
       </body>
     </html>
